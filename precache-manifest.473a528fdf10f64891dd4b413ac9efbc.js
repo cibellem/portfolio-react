3d45f20ff96c758fbc69d35c31bccd36821cd956
Cibelle Montor <you@example.com>
2020-06-13 17:45:03 -0700

    Updates

---
+++ precache-manifest.473a528fdf10f64891dd4b413ac9efbc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ea06ff9d7095b854aedd6e56a837b9",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "29727d81cdcee057fb48",
+    "url": "/portfolio-react/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e1a15c23688ed9803e41",
+    "url": "/portfolio-react/static/css/main.6a11c529.chunk.css"
+  },
+  {
+    "revision": "29727d81cdcee057fb48",
+    "url": "/portfolio-react/static/js/2.4feecd50.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio-react/static/js/2.4feecd50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1a15c23688ed9803e41",
+    "url": "/portfolio-react/static/js/main.350837ed.chunk.js"
+  },
+  {
+    "revision": "fb3203ba594dbdde600d",
+    "url": "/portfolio-react/static/js/runtime-main.0f44d30c.js"
+  },
+  {
+    "revision": "c46374213407f123621fe793f337efe1",
+    "url": "/portfolio-react/static/media/ck.c4637421.svg"
+  },
+  {
+    "revision": "41cf5b0456beb7c39ecd9ae6c7eff203",
+    "url": "/portfolio-react/static/media/me.41cf5b04.svg"
+  },
+  {
+    "revision": "be1e3fc30e4c362f82ca9518565a05b4",
+    "url": "/portfolio-react/static/media/pleasantly.be1e3fc3.svg"
+  },
+  {
+    "revision": "158a119c376f24fdc056b53e97a3a1b0",
+    "url": "/portfolio-react/static/media/shelfie.158a119c.svg"
+  },
+  {
+    "revision": "8664563baeefd64e8b2e86acc23beb62",
+    "url": "/portfolio-react/static/media/ws.8664563b.svg"
+  },
+  {
+    "revision": "1503d4ec45b7844c9153e47e16205221",
+    "url": "/portfolio-react/static/media/wx.1503d4ec.svg"
+  }
+]);
