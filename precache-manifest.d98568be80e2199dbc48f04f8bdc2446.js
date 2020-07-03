7c020b267cd43d15fb0c2e376f496830eb27f9fb
Cibelle Montor <montorcibelle@gmail.com>
2020-07-02 17:14:11 -0700

    Updates

---
+++ precache-manifest.d98568be80e2199dbc48f04f8bdc2446.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c5e893307f816ee6b63721df93ecb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f897ff611af0a354f950",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1bc9e57080cdb0e9e61b",
+    "url": "/static/css/main.b43ebf21.chunk.css"
+  },
+  {
+    "revision": "f897ff611af0a354f950",
+    "url": "/static/js/2.6b1233d6.chunk.js"
+  },
+  {
+    "revision": "08af06e3e9dc73ac1b25a0f709994794",
+    "url": "/static/js/2.6b1233d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc9e57080cdb0e9e61b",
+    "url": "/static/js/main.d90acc86.chunk.js"
+  },
+  {
+    "revision": "97ad9e083897a8286841",
+    "url": "/static/js/runtime-main.d7d8550f.js"
+  },
+  {
+    "revision": "c46374213407f123621fe793f337efe1",
+    "url": "/static/media/ck.c4637421.svg"
+  },
+  {
+    "revision": "c5ef0752f955bdc8c3fd307e34025583",
+    "url": "/static/media/me.c5ef0752.svg"
+  },
+  {
+    "revision": "fc8783270f1fcb4d2ec222f037ef555d",
+    "url": "/static/media/plantare.fc878327.svg"
+  },
+  {
+    "revision": "be1e3fc30e4c362f82ca9518565a05b4",
+    "url": "/static/media/pleasantly.be1e3fc3.svg"
+  },
+  {
+    "revision": "158a119c376f24fdc056b53e97a3a1b0",
+    "url": "/static/media/shelfie.158a119c.svg"
+  },
+  {
+    "revision": "1503d4ec45b7844c9153e47e16205221",
+    "url": "/static/media/wx.1503d4ec.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6cf70d5b63f433f1a9ffe1932ee6adb8.js"
+  "/precache-manifest.d98568be80e2199dbc48f04f8bdc2446.js"
 );
 
 self.addEventListener('message', (event) => {
