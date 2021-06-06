e4b2c5a137e8e88591ffbc47654028ef81e72e75
Cibelle Montor <montorcibelle@gmail.com>
2021-06-06 13:34:10 -0700

    adds google analitcs

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import HomePage from "./pages/home";
+import ReactGA from "react-ga";
 import Footer from "./components/footer";
 import NavBar from "./components/nav";
 import "./pages/style.scss";
+
+ReactGA.initialize(process.env.REACT_APP_GA);
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
